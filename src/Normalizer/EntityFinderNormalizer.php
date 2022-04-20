173d454c9da893a4d5499c1f1ce98ede922ccbf6
MartkCz <martk494@gmail.com>
2022-04-20 20:47:13 +0200

    fix checking if class type is entity

--- src/Normalizer/EntityFinderNormalizer.php
+++ src/Normalizer/EntityFinderNormalizer.php
@@ -2,6 +2,7 @@
 
 namespace WebChemistry\Serializer\Normalizer;
 
+use Doctrine\Common\Util\ClassUtils;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 
@@ -25,7 +26,12 @@ final class EntityFinderNormalizer implements DenormalizerInterface
 
 	public function supportsDenormalization($data, string $type, string $format = null): bool
 	{
-		if (!$this->em->getMetadataFactory()->hasMetadataFor($type)) {
+		if (str_ends_with($type, '[]')) {
+			return false;
+		}
+
+		$type = ClassUtils::getRealClass($type);
+		if ($this->em->getMetadataFactory()->isTransient($type)) {
 			return false;
 		}
 
@@ -33,7 +39,7 @@ final class EntityFinderNormalizer implements DenormalizerInterface
 			return true;
 		}
 
-		return is_scalar($data) && !str_ends_with($type, '[]');
+		return is_scalar($data);
 	}
 
 }
